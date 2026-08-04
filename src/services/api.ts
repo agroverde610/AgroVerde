1b612082790a9a34dc17ea15a4652e05325d9a2c
edisonparedes <edisonparedes242@gmail.com>
2026-08-04 00:07:45 -0500

    Actualización de api

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,9 @@ import type {
 } from '../interfaces/ICompra';
 
 const api = axios.create({
-    baseURL: 'https://agroverde-e9gdg9hbc8a2ctgc.mexicocentral-01.azurewebsites.net/api'
+   baseURL: 'https://agroverde-e9gdg9hbc8a2ctgc.mexicocentral-01.azurewebsites.net/api'
+    //baseURL: 'https://localhost:7145/api'
+
     // Ya NO forzamos 'Content-Type': 'application/json' aquí.
     // Axios ya pone 'application/json' automático cuando mandas un objeto normal,
     // y 'multipart/form-data; boundary=...' automático cuando mandas un FormData
